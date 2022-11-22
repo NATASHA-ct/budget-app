7ff2b2d3c91997380545340f2a7138a74dffc0ba
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-23 00:34:35 +0200

    Add associations for  categories

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
-    
+  belongs_to :user
+  has_many :group_expenses, dependent: :destroy
+  has_many :expensess, through: :group_expenses 
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
