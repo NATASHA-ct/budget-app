fb0e5b4499c4d989533e40c36ba5600a41e1b135
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-26 00:27:17 +0200

    Remove comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,10 +63,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
-  # def user_params
-  #   params.fetch(:user, {})
-  # end
   def user_params
     params.permit(:name, :email, :password, :password_confirmation)
   end
